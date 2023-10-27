f00ee29fa069f6607ea80c44989afe70e4bb0957
andreluissousa <andreborges1777@gmail.com>
2023-10-26 23:26:03 -0300

    Trabalhando com lista

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -50,7 +50,21 @@ const styles = StyleSheet.create({
     },
     exibitionResultImc: {
         width: "100%",
-        height: "50%"
+        //height: "50%"
+    },
+    listImcs: {
+        marginTop: 20
+    },
+    resultImcItem: {
+        fontSize: 26,
+        color: "red",
+        height: 50,
+        width: "100%",
+        paddingRight: 20
+    },
+    textResultItemList: {
+        fontSize: 14,
+
     }
 })
 
